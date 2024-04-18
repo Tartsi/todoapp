6f3347a120190844e13f5b0a16411437adfa6ece
Tartsi <tarvo.lilja@hotmail.fi>
2024-04-18 04:07:02 +0300

    Pylint fixes

--- src/app/views.py
+++ src/app/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
+    """Return the index-page of the application
+
+    Args:
+        request (HttpRequest): HTTP-request object from the browser
+
+    Returns:
+        HttpResponse: Renders and returns the 'index.html' template
+    """
+
     return render(request, 'index.html')
