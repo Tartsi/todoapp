9897706ca7178680209f9e0f45e6e651c400b024
Tartsi <tarvo.lilja@hotmail.fi>
2024-04-19 03:05:37 +0300

    Add frontend-check (JS) for login page form

--- frontend/static/index.js
+++ frontend/static/index.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function checkContent() {
+    
+    // Basic content check for login-form
+
+    const username = document.querySelector("#username").value.trim();
+    const password = document.querySelector("#password").value.trim();
+
+    function resetValues() {
+        document.querySelector("#username").value = "";
+        document.querySelector("#password").value = "";
+    }
+
+    if (username.length < 4) {
+        alert("Username not 4 characters!");
+        resetValues();
+        return false;
+    }
+
+    if (password.length < 4) {
+        alert("Password not 4 characters!");
+        resetValues();
+        return false;
+    }
+
+    if (username.includes(";") || username.includes("'") || username.includes("--")) {
+        alert("Invalid characters in username!");
+        resetValues();
+        return false;
+    }
+
+    return true;
+}
