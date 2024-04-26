bf7f4566045b4c938e5b87ecbc19b7ea652db61e
Tartsi <tarvo.lilja@hotmail.fi>
2024-04-26 03:15:44 +0300

    Add Task-model

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    task = models.CharField(max_length=200, verbose_name='Task')
+    completed = models.BooleanField(default=False, verbose_name='Completed')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Created At")
+
+    def __str__(self):
+        return self.task
