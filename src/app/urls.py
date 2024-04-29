744e1f7c50f2645e553f712d40cf36684e3ce69f
Tarvo Lilja <tarvo.lilja@hotmail.fi>
2024-04-29 15:55:31 +0300

    Add Login + Template for todo-page

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, register, add_user
+from .views import index, register, add_user, login_user
 
 urlpatterns = [
     path('', index, name='index'),
-    path('register/', register, name='register'),
-    path('add_user/', add_user, name='add_user'),
+    path('register', register, name='register'),
+    path('add_user', add_user, name='add_user'),
+    path('login_user', login_user, name='login_user'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model, authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 
 User = get_user_model()
@@ -70,3 +70,34 @@ def add_user(request):
     else:
         form = UserCreationForm()
         return render(request, 'register.html', {'form': form})
+
+
+def login_user(request):
+
+    # FIX THE ERROR MESSAGING HERE!
+
+    if request.method == 'POST':
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return render(request, 'listings.html')
+            else:
+                messages.error(request, "Invalid username or password")
+                return render(request, 'index.html', {'form': form})
+        else:
+            for field in form.errors:
+                for error in form.errors[field]:
+                    messages.error(request, error)
+
+            return render(request, 'index.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'index.html', {'form': form})
