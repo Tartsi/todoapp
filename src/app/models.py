9d9bb5ec9770e45be08d0249e1d6fc0723be9da0
Tarvo Lilja <tarvo.lilja@hotmail.fi>
2024-05-05 22:43:24 +0300

    Add User link to Tasks

--- src/app/models.py
+++ src/app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Task(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='User')
     task = models.CharField(max_length=200, verbose_name='Task')
     completed = models.BooleanField(default=False, verbose_name='Completed')
     created_at = models.DateTimeField(
