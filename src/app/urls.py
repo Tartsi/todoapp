887d8dbf3e3f1760af0b31d5a0c4cec3f5c9bd98
Tarvo Lilja <tarvo.lilja@hotmail.fi>
2024-05-05 23:35:16 +0300

    Rename functions

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import logout_then_login
-from .views import index, register, add_user, login_user, listings_view
+from .views import login, register_view, add_user, login_view, listings_view
 
 urlpatterns = [
-    path('', index, name='login'),
-    path('register', register, name='register'),
+    path('', login, name='login'),
+    path('register_view', register_view, name='register_view'),
     path('add_user', add_user, name='add_user'),
-    path('login_user', login_user, name='login_user'),
-    path('listings', listings_view, name='listings'),
+    path('login_view', login_view, name='login_view'),
+    path('listings_view', listings_view, name='listings_view'),
     path('logout', logout_then_login, name='logout'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model, authenticate, login
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from .models import Task
+from django.contrib.auth.decorators import login_required
 
 User = get_user_model()
 
 
-def index(request):
+def login(request):
     """Return the index-page of the application
 
     Args:
@@ -34,6 +36,63 @@ def register_view(request):
     """
 
     return render(request, 'register.html')
+
+
+def login_view(request):
+    """Logins a registered user to the application.
+
+    Args:
+        request (HttpRequest): HTTP-request object from the browser
+
+    Returns:
+        HttpResponse: Returns login-page with error messages if
+        login unsuccessful, else returns todo-page of the user
+    """
+
+    if request.method == 'POST':
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                auth_login(request, user)
+                return redirect('listings_view')
+
+            # Show appropriate error messages if login unsuccessful
+            messages.error(request, "Invalid username or password")
+            return render(request, 'index.html', {'form': form})
+
+        # Show appropriate error messages if login unsuccessful
+        for field in form.errors:
+            for error in form.errors[field]:
+                messages.error(request, error)
+
+        return render(request, 'index.html', {'form': form})
+
+    form = AuthenticationForm()
+    return render(request, 'index.html', {'form': form})
+
+
+@login_required(login_url='login')
+def listings_view(request):
+    """Renders the listings page with all the tasks of the logged-in user.
+
+    Args:
+        request (HttpRequest): HTTP-request object from the browser
+
+    Returns:
+        HttpResponse: Renders and returns the 'listings.html' template
+        with the tasks of the logged-in user
+    """
+
+    # Retrieve all tasks of the logged-in user
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'listings.html', {'tasks': tasks})
 
 
 def add_user(request):
@@ -74,46 +133,3 @@ def add_user(request):
     else:
         form = UserCreationForm()
         return render(request, 'register.html', {'form': form})
-
-
-def login_user(request):
-    """Logins a registered user to the application.
-
-    Args:
-        request (HttpRequest): HTTP-request object from the browser
-
-    Returns:
-        HttpResponse: Returns login-page with error messages if
-        login unsuccessful, else returns todo-page of the user
-    """
-
-    if request.method == 'POST':
-
-        form = AuthenticationForm(request, data=request.POST)
-
-        if form.is_valid():
-
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-
-                # TODO: Get user tasks here
-
-                return render(request, 'listings.html')
-
-            # Show appropriate error messages if login unsuccessful
-            messages.error(request, "Invalid username or password")
-            return render(request, 'index.html', {'form': form})
-
-        # Show appropriate error messages if login unsuccessful
-        for field in form.errors:
-            for error in form.errors[field]:
-                messages.error(request, error)
-
-        return render(request, 'index.html', {'form': form})
-
-    form = AuthenticationForm()
-    return render(request, 'index.html', {'form': form})
